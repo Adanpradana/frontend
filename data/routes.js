f727b0973f18abf62f457c2d6c1cb11ba9b61b18
Adanpradana <adanrizkahariz@gmail.com>
2024-04-07 18:16:31 +0700

    initial commit

---
+++ data/routes.js
@@ -0,0 +1,27 @@
+export const routes = [
+  {
+    id: 1,
+    name: "Reporting Pages",
+    path: "/",
+  },
+  {
+    id: 1,
+    name: "Student",
+    path: "/student",
+  },
+  {
+    id: 1,
+    name: "Book",
+    path: "/books",
+  },
+  {
+    id: 1,
+    name: "Book Category",
+    path: "/reporting",
+  },
+  {
+    id: 1,
+    name: "Transaction",
+    path: "/transaction",
+  },
+];
---
+++ src/components/layout/MainLayout.js
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router";
+import { routes } from "../../../data/routes";
+import Link from "next/link";
+
+export default function MainLayout({ children }) {
+  const router = useRouter();
+
+  return (
+    <div className="grid grid-cols-4 h-screen ">
+      <div className="bg-red-200 p-4 space-y-2 ">
+        {routes.map((route) => (
+          <div
+            key={route.id}
+            className={`bg-blue-500 ${
+              router.pathname === route.path && "bg-blue-600"
+            } hover:bg-blue-600 rounded-md`}
+          >
+            <Link href={route.path}>
+              <div className="p-4 text-[1.5rem] text-white uppercase text-center">
+                {route.name}
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className="col-span-3 bg-yellow-50">{children}</div>
+    </div>
+  );
+}

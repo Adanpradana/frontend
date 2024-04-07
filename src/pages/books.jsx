c8d5665cc6846bb5a8921cc895792423abbb09b2
Adanpradana <adanrizkahariz@gmail.com>
2024-04-07 21:20:45 +0700

    add book  pages

---
+++ src/pages/books.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import MainLayout from "@/components/layout/MainLayout";
+import { Button } from "@/components/ui/button";
+import ListTables from "@/components/ListTables";
+import useGetDataApi from "@/hooks/useGetDataApi";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Book() {
+  const label = "book";
+  const URL_API = `${process.env.NEXT_PUBLIC_API_HOST}/${label}`;
+  const { data } = useGetDataApi(URL_API);
+  const columns = [
+    "No",
+    "Book Title",
+    "Author",
+    "Year",
+    "Quantity",
+    "Category",
+  ];
+  const rows = data.map((item, index) => ({
+    id: item.id,
+    dynamicRows: {
+      no: index + 1,
+      name: item.title,
+      author: item.author,
+      year: item.year,
+      quantity: item.quantity,
+      category: item?.category?.category_name,
+    },
+  }));
+  console.log(data);
+  return (
+    <MainLayout>
+      <div className="container p-4 h-full">
+        <ListTables url={URL_API} columns={columns} label={label} rows={rows} />
+      </div>
+    </MainLayout>
+  );
+}
